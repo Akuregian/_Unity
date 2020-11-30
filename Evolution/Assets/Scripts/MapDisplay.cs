65adf44edc543baccb39b0ad8034536b2ecad4cd
Anthony Kuregian <AnthonyKuregian@yahoo.com>
2020-11-30 01:58:11 -0800

    Added Height

--- Evolution/Assets/Scripts/MapDisplay.cs
+++ Evolution/Assets/Scripts/MapDisplay.cs
@@ -1,16 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-// MapDisplay class is used to draw Color or Textures to the 2D noise map
-public class MapDisplay : MonoBehaviour
-{
-    public Renderer textureRender;
-    public void DrawTexture(Texture2D texture) {
-  
-        textureRender.sharedMaterial.mainTexture = texture;
-        textureRender.transform.localScale = new Vector3(texture.width, 1, texture.height);
-    }
+public class MapDisplay : MonoBehaviour {
 
+	public Renderer textureRender;
+	public MeshFilter meshFilter;
+	public MeshRenderer meshRenderer;
+
+	public void DrawTexture(Texture2D texture) {
+		textureRender.sharedMaterial.mainTexture = texture;
+		textureRender.transform.localScale = new Vector3(texture.width, 1, texture.height);
+	}
+
+	public void DrawMesh(MeshData meshData, Texture2D texture) {
+		meshFilter.sharedMesh = meshData.CreateMesh();
+		meshRenderer.sharedMaterial.mainTexture = texture;
+	}
 
 }
--- Evolution/Assets/Scripts/MapGenerator.cs
+++ Evolution/Assets/Scripts/MapGenerator.cs
@@ -1,74 +1,75 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-// MapGenerator Class used the noise class, that returns a 2D PerlinNoise Map and then
-// used MapDisplay class to draw a texture to that map
 public class MapGenerator : MonoBehaviour {
 
-    public enum DrawMode { NoiseMap, ColorMap }
-    public DrawMode drawMode;
-    public int mapWidth;
-    public int mapHeight;
-    public float noiseScale;
-    public int octaves;
-    [Range(0,1)]
-    public float persistance;
-    public float lacunarity;
-    public int seed;
-    public Vector2 offSet;
+	public enum DrawMode { NoiseMap, ColourMap, Mesh };
+	public DrawMode drawMode;
 
-    public bool autoUpdate;
+	public int mapWidth;
+	public int mapHeight;
+	public float noiseScale;
+	public float meshHeightMapMulitplier;
 
-    public TerrainType[] regions;
+	public int octaves;
+	[Range(0, 1)]
+	public float persistance;
+	public float lacunarity;
 
-    public void GenerateMap() {
-        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offSet); // Generate noiseMap
+	public int seed;
+	public Vector2 offset;
 
-        Color[] colorMap = new Color[mapWidth*mapHeight];
-        for (int y = 0; y < mapHeight; y++) {
-            for (int x = 0; x < mapWidth; x++) {
-                float currentHeight = noiseMap[x, y];
-                for (int i = 0; i < regions.Length; i++) {
-                    if (currentHeight <= regions[i].height) {
-                        colorMap[y * mapWidth + x] = regions[i].color;
-                        break;
-                    }
-                }
-            }
-        }
+	public bool autoUpdate;
 
-        MapDisplay display = FindObjectOfType<MapDisplay>(); // Find the MapDisplay class, to display Texure
-        if (drawMode == DrawMode.NoiseMap) {
-            display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));  // Draws colors/textures
-        } else if(drawMode == DrawMode.ColorMap) {
-            display.DrawTexture(TextureGenerator.TextureFromColorMap(colorMap, mapWidth, mapHeight));  // Draws colors/textures
-        }
+	public TerrainType[] regions;
 
+	public void GenerateMap() {
+		float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
+
+		Color[] colourMap = new Color[mapWidth * mapHeight];
+		for (int y = 0; y < mapHeight; y++) {
+			for (int x = 0; x < mapWidth; x++) {
+				float currentHeight = noiseMap[x, y];
+				for (int i = 0; i < regions.Length; i++) {
+					if (currentHeight <= regions[i].height) {
+						colourMap[y * mapWidth + x] = regions[i].color;
+						break;
+					}
+				}
+			}
+		}
+
+		MapDisplay display = FindObjectOfType<MapDisplay>(); 
+		if (drawMode == DrawMode.NoiseMap) {
+			display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+		}
+		else if (drawMode == DrawMode.ColourMap) {
+			display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+		}
+		else if (drawMode == DrawMode.Mesh) {
+			display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap, meshHeightMapMulitplier), TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+		}
+	}
+
+	void OnValidate() {
+		if (mapWidth < 1) {
+			mapWidth = 1;
+		}
+		if (mapHeight < 1) {
+			mapHeight = 1;
+		}
+		if (lacunarity < 1) {
+			lacunarity = 1;
+		}
+		if (octaves < 0) {
+			octaves = 0;
+		}
+	}
+
+    [System.Serializable]
+    public struct TerrainType {
+        public string name;
+        public float height;
+        public Color color;
     }
-
-    private void OnValidate() { // Function from Unity that is ran everytime the scipt is loaded or a value in the inspector changed
-        if(mapWidth < 1) {
-            mapWidth = 1;
-        }
-        
-        if(mapHeight < 1) {
-            mapHeight = 1;
-        }
-
-        if(lacunarity < 1) {
-            lacunarity = 1;
-        }
-
-        if(octaves < 0) {
-            octaves = 0;
-        }
-    }
-}
-
-[System.Serializable]
-public struct TerrainType {
-    public string name;
-    public float height;
-    public Color color;
 }
